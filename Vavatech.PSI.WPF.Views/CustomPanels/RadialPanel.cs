20cb1ebb2b1bac54ac7540a48f09d0b7067921b5
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2018-07-20 15:48:26 +0200

    Dodanie własnego panelu

---
+++ Vavatech.PSI.WPF.Views/CustomPanels/RadialPanel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Vavatech.PSI.WPF.Views.CustomPanels
+{
+    public class RadialPanel : Panel
+    {
+
+        // Measure each children and give as much room as they want 
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            foreach (UIElement element in Children)
+            {
+                //Give Infinite size as the avaiable size for all the children
+                element.Measure(new Size(double.PositiveInfinity, double.PositiveInfinity));
+
+            }
+
+            return base.MeasureOverride(availableSize);
+
+        }
+
+
+
+        //Arrange all children based on the geometric equations for the circle.
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (Children.Count == 0)
+                return finalSize;
+
+            double _angle = 0;
+
+            // Degrees converted to Radian by multiplying with PI/180
+            double _incrementalAngularSpace = (360.0 / Children.Count) * (Math.PI / 180);
+
+            //An approximate radii based on the avialable size , obviusly a better approach is needed here.
+            double radiusX = finalSize.Width / 2.4;
+            double radiusY = finalSize.Height / 2.4;
+
+            foreach (UIElement element in Children)
+            {
+                // Calculate the point on the circle for the element
+                Point childPoint = new Point(Math.Cos(_angle) * radiusX, -Math.Sin(_angle) * radiusY);
+
+                // Offsetting the point to the Avalable rectangular area which is FinalSize.
+                Point actualChildPoint = new Point(finalSize.Width / 2 + childPoint.X - element.DesiredSize.Width / 2, finalSize.Height / 2 + childPoint.Y - element.DesiredSize.Height / 2);
+
+                // Call Arrange method on the child element by giving the calculated point as the placementPoint.
+                element.Arrange(new Rect(actualChildPoint.X, actualChildPoint.Y, element.DesiredSize.Width, element.DesiredSize.Height));
+
+                //Calculate the new _angle for the next element
+                _angle += _incrementalAngularSpace;
+            }
+
+
+
+            return finalSize;
+
+        }
+
+    }
+}
